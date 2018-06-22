1d1031569e6e62ed4bd516b10ff397c6d46c0639
Albrecht Köhnlein <ak@koehnlein.eu>
2018-06-22 10:12:50 +0200

    Add composer binary to all images

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker build --pull . -f Dockerfile.php56-alpine -t akoehnlein/typo3:php56-alpine && \
-docker build --pull . -f Dockerfile.php70-alpine -t akoehnlein/typo3:php70-alpine && \
-docker build --pull . -f Dockerfile.php71-alpine -t akoehnlein/typo3:php71-alpine && \
-docker build --pull . -f Dockerfile.php72-alpine -t akoehnlein/typo3:php72-alpine && \
-docker build --pull . -f Dockerfile.php56 -t akoehnlein/typo3:php56 && \
-docker build --pull . -f Dockerfile.php70 -t akoehnlein/typo3:php70 && \
-docker build --pull . -f Dockerfile.php71 -t akoehnlein/typo3:php71 && \
-docker build --pull . -f Dockerfile.php72 -t akoehnlein/typo3:php72
+docker build --pull . -f Dockerfile.php56-alpine -t akoehnlein/typo3:php56-alpine && docker run -it -v `pwd`/tests.sh:/tmp/test.sh --entrypoint "ash" akoehnlein/typo3:php56-alpine /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php70-alpine -t akoehnlein/typo3:php70-alpine && docker run -it -v `pwd`/tests.sh:/tmp/test.sh --entrypoint "ash" akoehnlein/typo3:php70-alpine /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php71-alpine -t akoehnlein/typo3:php71-alpine && docker run -it -v `pwd`/tests.sh:/tmp/test.sh --entrypoint "ash" akoehnlein/typo3:php71-alpine /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php72-alpine -t akoehnlein/typo3:php72-alpine && docker run -it -v `pwd`/tests.sh:/tmp/test.sh --entrypoint "ash" akoehnlein/typo3:php72-alpine /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php56 -t akoehnlein/typo3:php56 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/typo3:php56 /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php70 -t akoehnlein/typo3:php70 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/typo3:php70 /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php71 -t akoehnlein/typo3:php71 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/typo3:php71 /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php72 -t akoehnlein/typo3:php72 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/typo3:php72 /tmp/test.sh
---
+++ tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# some commands must be executable
+composer --version || exit 1
+
+# some PHP extensions must be loaded
+echo 'search mysqli' && php -i | grep '^mysqli$' || exit 1
+echo 'search xdebug' && php -i | grep '^xdebug$' || exit 1
