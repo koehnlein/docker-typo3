25ebc5a563fd938c87c523ef30f0c84c8bfab166
Albrecht Köhnlein <ak@koehnlein.eu>
2018-08-15 12:43:16 +0200

    Add notice when user tries to run 'bash' inside alpine images

--- tests.sh
+++ tests.sh
@@ -2,6 +2,7 @@
 
 # some commands must be executable
 composer --version || exit 1
+bash || exit 1
 
 # some PHP extensions must be loaded
 echo 'search mysqli' && php -i | grep '^mysqli$' || exit 1
