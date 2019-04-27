3f5eb5fce328924c8769166e9a667441784b6e34
Albrecht Köhnlein <ak@koehnlein.eu>
2019-04-27 11:58:49 +0200

    Clean up test script

--- tests.sh
+++ tests.sh
@@ -2,7 +2,6 @@
 
 # some commands must be executable
 COMPOSER_ALLOW_SUPERUSER=1 composer --version || exit 1
-bash || exit 1
 
 # some PHP extensions must be loaded
 echo 'search mysqli' && php -i | grep '^mysqli$' || exit 1
