946c24ec34e584e8096e228f1f010423d0271d68
Albrecht Köhnlein <ak@koehnlein.eu>
2019-05-01 19:23:14 +0200

    Add PHP 7.3 images

--- README.md
+++ README.md
@@ -24,16 +24,18 @@ See <https://hub.docker.com/r/akoehnlein/typo3/> for compiled images:
 
 The following images are based on the official PHP images, which are based on Debian Linux images
 
-* `akoehnlein/typo3:php56`
-* `akoehnlein/typo3:php70`
-* `akoehnlein/typo3:php71`
+* `akoehnlein/typo3:php73`
 * `akoehnlein/typo3:php72`
+* `akoehnlein/typo3:php71`
+* `akoehnlein/typo3:php70`
+* `akoehnlein/typo3:php56`
 
 ### Alpine
 
 There are also alternative images, based on the smaller Alpine Linux images
 
-* `akoehnlein/typo3:php56-alpine`
-* `akoehnlein/typo3:php70-alpine`
-* `akoehnlein/typo3:php71-alpine`
+* `akoehnlein/typo3:php73-alpine`
 * `akoehnlein/typo3:php72-alpine`
+* `akoehnlein/typo3:php71-alpine`
+* `akoehnlein/typo3:php70-alpine`
+* `akoehnlein/typo3:php56-alpine`
--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-docker build --pull . -f Dockerfile.php56-alpine -t akoehnlein/typo3:php56-alpine && docker run -it -v `pwd`/tests.sh:/tmp/test.sh --entrypoint "ash" akoehnlein/typo3:php56-alpine /tmp/test.sh && \
-docker build --pull . -f Dockerfile.php70-alpine -t akoehnlein/typo3:php70-alpine && docker run -it -v `pwd`/tests.sh:/tmp/test.sh --entrypoint "ash" akoehnlein/typo3:php70-alpine /tmp/test.sh && \
-docker build --pull . -f Dockerfile.php71-alpine -t akoehnlein/typo3:php71-alpine && docker run -it -v `pwd`/tests.sh:/tmp/test.sh --entrypoint "ash" akoehnlein/typo3:php71-alpine /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php73-alpine -t akoehnlein/typo3:php73-alpine && docker run -it -v `pwd`/tests.sh:/tmp/test.sh --entrypoint "ash" akoehnlein/typo3:php73-alpine /tmp/test.sh && \
 docker build --pull . -f Dockerfile.php72-alpine -t akoehnlein/typo3:php72-alpine && docker run -it -v `pwd`/tests.sh:/tmp/test.sh --entrypoint "ash" akoehnlein/typo3:php72-alpine /tmp/test.sh && \
-docker build --pull . -f Dockerfile.php56 -t akoehnlein/typo3:php56 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/typo3:php56 /tmp/test.sh && \
-docker build --pull . -f Dockerfile.php70 -t akoehnlein/typo3:php70 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/typo3:php70 /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php71-alpine -t akoehnlein/typo3:php71-alpine && docker run -it -v `pwd`/tests.sh:/tmp/test.sh --entrypoint "ash" akoehnlein/typo3:php71-alpine /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php70-alpine -t akoehnlein/typo3:php70-alpine && docker run -it -v `pwd`/tests.sh:/tmp/test.sh --entrypoint "ash" akoehnlein/typo3:php70-alpine /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php56-alpine -t akoehnlein/typo3:php56-alpine && docker run -it -v `pwd`/tests.sh:/tmp/test.sh --entrypoint "ash" akoehnlein/typo3:php56-alpine /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php73 -t akoehnlein/typo3:php73 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/typo3:php73 /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php72 -t akoehnlein/typo3:php72 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/typo3:php72 /tmp/test.sh && \
 docker build --pull . -f Dockerfile.php71 -t akoehnlein/typo3:php71 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/typo3:php71 /tmp/test.sh && \
-docker build --pull . -f Dockerfile.php72 -t akoehnlein/typo3:php72 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/typo3:php72 /tmp/test.sh
+docker build --pull . -f Dockerfile.php70 -t akoehnlein/typo3:php70 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/typo3:php70 /tmp/test.sh && \
+docker build --pull . -f Dockerfile.php56 -t akoehnlein/typo3:php56 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/typo3:php56 /tmp/test.sh && \
+echo "done"
