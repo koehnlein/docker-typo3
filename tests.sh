a5766a4abfebc1d8274e08b00fa6a73ef929658c
Albrecht Köhnlein <ak@koehnlein.eu>
2019-09-20 10:45:43 +0200

    Add APCu

--- tests.sh
+++ tests.sh
@@ -8,6 +8,7 @@ COMPOSER_ALLOW_SUPERUSER=1 composer --version || exit 1
 # some PHP extensions must be loaded
 echo 'search mysqli' && php -i | grep '^mysqli$' || exit 1
 #echo 'search xdebug' && php -i | grep '^xdebug$' || exit 1
+echo 'search apcu' && php -i | grep '^apcu$' || exit 1
 echo 'search blackfire' && php -i | grep '^blackfire$' || exit 1
 
 #echo 'xdebug must be disabled by PHP_XDEBUG_ENABLED=0'
